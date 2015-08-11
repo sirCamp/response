20c01901296aebee87abec9ca367d68e2df71ea3
sirCamp <sircampydevolp@gmail.com>
2015-08-11 11:15:28 +0200

    structure changed

--- tests/test.php
+++ tests/test.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-require_once __DIR__ . '/../src/Response/InfoResponse.php';
+use Sircamp\Response\InfoResponse as InfoResponse;
 
 $info = new InfoResponse(array(),"ok");
 echo $info->message;
